ee6d6ecbf53daee7fc2b60ba742ab1ad63592ad9
Tracy Fussell <tracy.fussell@gmail.com>
2018-08-26 19:03:43 -0400

    Add first two tests for name and url

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,12 +32,23 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('have a url and it is not empty',function() {
+           for(let feed of allFeeds){
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           }});
 
+           it('have a name and it is not empty',function() {
+             for(let feed of allFeeds){
+               expect(feed.name).toBeDefined();
+               expect(feed.name.length).not.toBe(0);
+             }});
+});
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
+
 
 
     /* TODO: Write a new test suite named "The menu" */
